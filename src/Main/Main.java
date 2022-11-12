087e245c1a49e863d448e3f8d79dacf594cf8eb3
Evo124 <gabrielnazate124@gmail.com>
2022-11-12 16:09:38 -0500

    Ingreso de credenciales, Gabriel

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,21 @@
 package Main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        //System.out.println("Hola");h
-        System.out.println("Hello world2!");
+
+        Scanner ingreso = new Scanner (System.in);
+
+        System.out.println("Ingrese su número de tarjeta: ");
+
+        String numero = ingreso.next();
+
+        System.out.println("Ingrese su clave: ");
+
+        int clave = ingreso.nextInt();
+
     }
 }
