19226c314adb8c99f011e907b03a3337345f37f6
Evo124 <gabrielnazate124@gmail.com>
2022-11-12 17:08:48 -0500

    Consulta de saldo, Gabriel

--- src/Main/Tarjeta.java
+++ src/Main/Tarjeta.java
@@ -1,6 +1,6 @@
 package Main;
 
-import java.util.ArrayList;
+
 import java.util.HashMap;
 
 public class Tarjeta {
@@ -8,7 +8,7 @@ public class Tarjeta {
     //ArrayList<String> claves;
     HashMap<String, String> tarjetas;
     public Tarjeta() {
-        tarjetas = new HashMap<String, String>();
+        tarjetas = new HashMap<>();
         tarjetas.put("4271900557374142", "123");
         tarjetas.put("4716050069748111", "672");
         tarjetas.put("4485305457933389", "345");
@@ -18,17 +18,13 @@ public class Tarjeta {
 
     //Metodo de consulta con la base de datos
     public boolean validarTarjeta(String numeroTarjeta, String clave) {
-        System.out.println("aa"+tarjetas.get(numeroTarjeta));
 
         if(tarjetas.containsKey(numeroTarjeta)){//consultar en BD
-            if(tarjetas.get(numeroTarjeta).equals(clave)){
-                return true;
-            }
+            return tarjetas.get(numeroTarjeta).equals(clave);
         }else{
             return false;
         }
         //borrar
-        return false;
     }
 
 }
