b53defba3d91c6cd7b0f69eb7f2aa8a16c519df9
Evo124 <gabrielnazate124@gmail.com>
2022-11-12 16:54:07 -0500

    Ingreso de credenciales, Gabriel

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        Tarjeta tarjeta = new Tarjeta();
 
         Scanner ingreso = new Scanner(System.in);
 
@@ -15,25 +14,29 @@ public class Main {
 
         System.out.println("Ingrese su clave: ");
 
-        int clave = ingreso.nextInt();
+        String clave = ingreso.next();
+        if (tarjeta.validarTarjeta(numero, clave)) {
+            int opc;
+            do {
+                System.out.println("\tMENU");
+                System.out.println("1. Consultar Saldo");
+                System.out.println("2. Salir");
+                opc = ingreso.nextInt();
+                switch (opc) {
+                    case 1:
 
-        int opc;
-        do {
-            System.out.println("\tMENU");
-            System.out.println("1. Consultar Saldo");
-            System.out.println("2. Salir");
-            opc = ingreso.nextInt();
-            switch (opc) {
-                case 1:
-
-                    break;
-                case 2:
-                    System.out.println("\tGracias por usar nuestros servicios");
-                    break;
-                default:
-                    System.out.println("Ingrese una opcion valida");
-                    break;
-            }
-        } while (opc != 2);
+                        break;
+                    case 2:
+                        System.out.println("\tGracias por usar nuestros servicios");
+                        break;
+                    default:
+                        System.out.println("Ingrese una opcion valida");
+                        break;
+                }
+            } while (opc != 2);
+        } else {
+            System.out.println("Credenciales erroneas");
+            System.out.println("\tGracias por usar nuestros servicios");
+        }
     }
 }
---
+++ src/Main/Tarjeta.java
@@ -0,0 +1,35 @@
+package Main;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Tarjeta {
+    //ArrayList<String> tarjetas;
+    //ArrayList<String> claves;
+    HashMap<String, String> tarjetas;
+    public Tarjeta() {
+        tarjetas = new HashMap<String, String>();
+        tarjetas.put("4271900557374142", "123");
+        tarjetas.put("4716050069748111", "672");
+        tarjetas.put("4485305457933389", "345");
+        tarjetas.put("4278174463982835", "987");
+        tarjetas.put("27", "12");
+    }
+
+    //Metodo de consulta con la base de datos
+    public boolean validarTarjeta(String numeroTarjeta, String clave) {
+        System.out.println("numeroo "+numeroTarjeta);
+        if(tarjetas.containsKey(numeroTarjeta)){//consultar en BD
+            if(tarjetas.get(numeroTarjeta) == clave){
+                System.out.println("numeroo "+numeroTarjeta);
+                System.out.println("Clave "+clave);
+                return true;
+            }
+        }else{
+            return false;
+        }
+        //borrar
+        return false;
+    }
+
+}
